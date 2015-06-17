ca82071e62b32a1014ba0e210ce780684958604c
Josh Voss <joshsvoss@gmail.com>
2015-06-17 18:03:08 +0300

    file verification done in main, constructor in Method.:

--- main/Sjavac.java
+++ main/Sjavac.java
@@ -1,10 +1,31 @@
 package main;
 
+import java.io.File;
+
 public class Sjavac {
 	
+	private static final int IO_FAILURE_NUM = 2;
+	private static final int ILLEGAL_CODE_NUM = 1;
+	private static final int KOSHER_CODE_NUM = 0;
+	private static final int SRC_FILE_INDEX = 0;
+
 	public static void main(String[] args) {
 		
 		// Make sure we received the necessary arguments:
+		if (args.length < 1) {
+			System.out.println(IO_FAILURE_NUM);
+			System.err.println("ERROR: no command line arg provided.");
+			System.err.println("USAGE: please provide source file path as cmd line argument.");
+			return;
+		}
+		
+		// Verify that the file exists and can be read:
+		File srcFile = new File(args[SRC_FILE_INDEX]);
+		if (!srcFile.exists() || !srcFile.canRead()) {
+			System.out.println(IO_FAILURE_NUM);
+			System.err.println("ERROR: file doesn't exist or can't be read.");
+			return;
+		}
 		
 	}
 
--- scopes/Method.java
+++ scopes/Method.java
@@ -4,10 +4,46 @@ import types.Type;
 
 public class Method extends Scope{
 	private Type[] argArray;
+	private String name;
+
+
+	public Method(String line) {
+		checkSyntax(line);
+		
+		updateLogic();
+		
+		
+	}
 	
 	
+	
+	
+	private void updateLogic() {
+		
+		
+	}
+
+
+
+
+	private void checkSyntax(String line) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
 	public Type[] getArgs() {
 		
+	}
+	
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
 	}
 
 }
