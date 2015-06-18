4b06a189cfbf8d2ff3b20bf31c4be94373f6d698
shana-m <shana.milstein@gmail.com>
2015-06-18 15:47:42 +0300

    updated parser with regexes

--- main/Parser.java
+++ main/Parser.java
@@ -3,12 +3,50 @@ package main;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Parser extends Sjavac {
 	
+	// The source file.
 	private File srcFile;
+	
+	// The file scanner.
 	private Scanner scanner;
 	
+	// The current line.
+	private String currLn;
+	
+	// A line counter.
+	private int lineCtr = 0;
+	
+	// The regex for the start of a variable declaration.
+	// TODO didn't deal with variable assignment.
+	public static final String varDec = "([final]++\\s)*+[int|boolean|char|double|String]++\\s[a-zA-Z]{1}+[\\w]*+.*";
+	
+	// The regex for method declaration.
+	// TODO didn't deal with method parameters, just that they may or may not exist.
+	public static final String methDec = "[void]++\\s[a-zA-Z]{1}+[\\w]*+\\s[(]{1}+[\\w|\\s]*+([)][;]){1}+\\s*";
+	
+	// The regex for a method call.
+	// TODO didn't deal with method parameters, just that they may or may not exist.
+	public static final String methCall = "\\s*+[a-zA-Z]{1}+[\\w]*+\\s[(]{1}+[\\w|\\s]*+([)][;]){1}+\\s*";
+	
+	// The regex for if/while loop.
+	// TODO didn't deal with loop parameters, just that they exist.
+	public static final String loop = "\\s*+[if|while]++\\s*+[(]{1}+\\s*+[\\w|\\s]++\\s*+([)]+\\s*+[{]+){1}+\\s*";
+	
+	// The regex for a comment.
+	public static final String doc = "\\++.*";
+	
+	// The regex for white space.
+	public static final String whiteSpace = "\\s*";
+	
+	/**
+	 * Construct a parser for the given file.
+	 * 
+	 * @param file, the file to be parsed.
+	 * @throws FileNotFoundException
+	 */
 	public Parser(File file) throws FileNotFoundException{
 		// We know the file has been verified in the main,
 		// So we can assume it is readable and exists.  
@@ -17,8 +55,23 @@ public class Parser extends Sjavac {
 	}
 	
 	
+	/**
+	 * Read the file line by line, sending each line to the line factory.
+	 */
 	public void readCode() {
-		
+		while(scanner.hasNext()){
+			currLn = scanner.nextLine();
+			lineCtr++;
+			
+			if(Pattern.matches(currLn, doc)){
+				continue;
+			}else if(Pattern.matches(currLn, varDec)){
+				// Send currLn to type factory.	
+			}else if(Pattern.matches(currLn, methDec)){
+				// Send currLn to scope Factory.
+			}
+			
+		}
 	}
 
 }
