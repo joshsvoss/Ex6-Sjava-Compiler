90e7437b0b37a016a47608005eea1140b309221a
shana-m <shana.milstein@gmail.com>
2015-06-21 14:52:53 +0300

    started on scope skeleton

--- main/Parser.java
+++ main/Parser.java
@@ -3,9 +3,10 @@ package main;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Parser extends Sjavac {
+public class Parser {
 	
 	// The source file.
 	private File srcFile;
@@ -19,26 +20,44 @@ public class Parser extends Sjavac {
 	// A line counter.
 	private int lineCtr = 0;
 	
-	// The regex for the start of a variable declaration. (Deals with cases with/out final, and with/out assignment.)
-	public static final String varDec = "\\s*+([f][i][n][a][l]++\\s+)?+[int|boolean|char|double|String]++\\s[a-zA-Z_]{1}+\\w*+\\s*+([=]++\\s*+\\S+)?\\s*+[;]++\\s*";
+	// The regex for the a variable declaration. (Deals with cases with/out final, and with/out assignment.)
+	public static final String varDec = "\\s*+(final{1}+\\s{1})?+(int|boolean|char|double|String){1}+\\s[a-zA-Z_]{1}+\\w*+\\s*+([=]{1}+\\s*+\\S+)?\\s*+[;]{1}+\\s*";
+	
+	// The regex for variable (re)assignment.
+	public static final String varAss = "\\s*+[a-zA-Z_]{1}+\\w*+\\s*+([=]{1}+\\s*+\\S+)?\\s*+[;]{1}+\\s*";
 	
 	// The regex for method declaration.
 	// TODO didn't deal with method parameters, just that they may or may not exist.
-	public static final String methDec = "[v][o][i][d]++\\s[a-zA-Z]{1}+[\\w]*+\\s[(]{1}+[\\w|\\s]*+([)][;]){1}+\\s*";
+	public static final String methDec = "void{1}+\\s[a-zA-Z]{1}+[\\w]*+\\s[(]{1}+(\\w|\\s)*+([)][;]){1}+\\s*";
 	
 	// The regex for a method call.
 	// TODO didn't deal with method parameters, just that they may or may not exist.
-	public static final String methCall = "\\s*+[a-zA-Z]{1}+[\\w]*+\\s[(]{1}+[\\w|\\s]*+([)][;]){1}+\\s*";
+	public static final String methCall = "\\s*+[a-zA-Z]{1}+[\\w]*+\\s[(]{1}+(\\w|\\s)*+([)][;]){1}+\\s*";
+	
+	// The regex for the method parameters.
+	public static final String methParam = "(\\s*+(final{1}+\\s{1})?+(int|boolean|char|double|String){1}+\\s[a-zA-Z_]{1}+\\w*+\\s*+([,]{1}+\\s*+(final{1}+\\s{1})?+(int|boolean|char|double|String){1}+\\s[a-zA-Z_]{1}+\\w*+\\s*)*)*";
 	
 	// The regex for if/while loop.
 	// TODO didn't deal with loop parameters, just that they exist.
-	public static final String loop = "\\s*+[if|while]++\\s*+[(]{1}+\\s*+[\\w|\\s]++\\s*+([)]+\\s*+[{]+){1}+\\s*";
+	public static final String loop = "\\s*+(if|while){1}+\\s*+[(]{1}+\\s*+(\\w|\\s)++\\s*+([)]+\\s*+[{]+){1}+\\s*";
+	
+	// The regex for the loop parameters.
+	public static final String loopParam = "\\s*+(true|false|([a-zA-Z_]{1}+\\w*)|([-]?+[0-9]++([.]{1}+[0-9]+)?)){1}+\\s*+(([|][|]|[&][&]){1}+\\s*+(true|false|([a-zA-Z_]{1}+\\w*)|([-]?+[0-9]++([.]{1}+[0-9]+)?)){1}+\\s*)*\\s*";
 	
 	// The regex for a comment.
-	public static final String doc = "\\++.*";
+	public static final String doc = "//{1}+.*";
 	
 	// The regex for white space.
 	public static final String whiteSpace = "\\s*";
+	
+	// The regex for a method return.
+	public static final String methEnd = "\\s*+return{1}+\\s*+[;]{1}+\\s*";
+	
+	// The regex for closing a scope.
+	public static final String scopeClose = "\\s*+[}]{1}+\\s*";
+	
+	// Scope depth counter.
+	private int depth = 0;
 	
 	/**
 	 * Construct a parser for the given file.
@@ -62,12 +81,24 @@ public class Parser extends Sjavac {
 			currLn = scanner.nextLine();
 			lineCtr++;
 			
-			if(Pattern.matches(currLn, doc)){
+			// If the currLn is documentation, a blank line, method call, or method return, continue.
+			if(Pattern.matches(currLn, doc)||Pattern.matches(currLn, whiteSpace)||Pattern.matches(currLn, methCall)||Pattern.matches(currLn, methEnd)){
 				continue;
 			}else if(Pattern.matches(currLn, varDec)){
 				// Send currLn to type factory.	
 			}else if(Pattern.matches(currLn, methDec)){
-				// Send currLn to scope Factory.
+				this.depth++;
+				// Send currLn and depth to scope Factory.
+			}else if(Pattern.matches(currLn, loop)){
+				this.depth++;
+				// Send currLn and depth to scope Factory.
+			}else if(Pattern.matches(currLn, scopeClose)){
+				this.depth--;
+				continue;
+			}else if(Pattern.matches(currLn, varAss)){
+				// Update the variable value.
+			}else{
+				// Throw syntax error.
 			}
 			
 		}
--- scopes/Scope.java
+++ scopes/Scope.java
@@ -1,5 +1,32 @@
 package scopes;
 
 public abstract class Scope {
-
+	
+	// The data for the current scope.
+	protected String scopeInit;
+	
+	// The depth of the current scope.
+	protected int depth;
+	
+	// The current scope's params.
+	protected String[] params;
+	
+	// Regex to single out the scope params.
+	protected static final String findParams = "(\\s*.*[(]{1}\\s*)|(\\s*[)]{1}.*\\s*)"; 
+	
+	public Scope(String currLn, int depth){
+		this.scopeInit = currLn;
+		this.depth = depth;
+		this.params = currLn.split(findParams);
+	}
+	
+	public abstract void updateLogic();
+	
+	public abstract boolean checkParamLogic(String params);
+	
+	public abstract String getName();
+	
+	public int getdepth(){
+		return this.depth;
+	}
 }
