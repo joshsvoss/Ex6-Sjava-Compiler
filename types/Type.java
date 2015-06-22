11531ddcc7dccf6b6c14b637c9e9f5724e6fb237
Josh Voss <joshsvoss@gmail.com>
2015-06-22 19:43:55 +0300

    Stam commented and deleted stuff in the types class.:

--- types/Type.java
+++ types/Type.java
@@ -5,11 +5,11 @@ public abstract class Type {
 	// Common fields to all of the types
 	private int depth;  //TODO need this with the vector of symbol tables?
 	private boolean isFinal;
-	private boolean isInitialized;
+	private boolean isInitialized; //TODO change this to method?
 	private boolean isLocallyInitialized; //TODO remember what this is for?
 	
-	public Type(String codeLine, int depth, boolean isFinal, boolean isInitialized,
-			boolean isLocallyInitialized) {
+	public Type(int depth, boolean isFinal,
+			boolean isLocallyInitialized) { //TODO should isLocallyInitialized be a method too?
 		
 		this.depth = depth;
 		this.isFinal = isFinal;
