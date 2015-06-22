9762062c18273b51e13372b9c7025704c1b57dbe
Josh Voss <joshsvoss@gmail.com>
2015-06-22 14:54:34 +0300

    added fields to the Type parent type

--- types/Type.java
+++ types/Type.java
@@ -3,7 +3,10 @@ package types;
 public abstract class Type {
 	
 	// Common fields to all of the types
-	int depth;
+	private int depth;  //TODO need this with the vector of symbol tables?
+	private boolean isFinal;
+	private boolean isInitialized;
+	private boolean isLocallyInitialized; //TODO remember what this is for?
 	
 	public Type(String line, int depth) {
 		
