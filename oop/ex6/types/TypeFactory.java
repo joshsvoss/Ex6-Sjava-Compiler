a96b027fde321a577e75af9534cc9f577313dc59
Josh Voss <joshsvoss@gmail.com>
2015-06-24 17:23:07 +0300

    Started adding javadocs

--- oop/ex6/types/TypeFactory.java
+++ oop/ex6/types/TypeFactory.java
@@ -2,10 +2,26 @@ package oop.ex6.types;
 
 import oop.ex6.main.SJavacException;
 
+/** This class has the job of received a string that defines what type to create,
+ * and trying to create an instance of the corresponding type.
+ * 
+ * If the String passed in does not match anything, an InvalidTypeException is thrown.
+ * @author Joshua Voss
+ *
+ */
 public class TypeFactory {
 
 	private boolean isFinal = false;
 	
+	/** This  method returns the correct Type based on the type String.
+	 * @param finalStr the part of the line where "final" would be
+	 * @param type substring with the typename
+	 * @param name substring with the name of the variable.
+	 * @param value substring with the value assigned to the variable.
+	 * @param depth the depth in the stack that this 
+	 * @return a reference to an instance of the correct subtype of Type
+	 * @throws SJavacException
+	 */
 	public Type generateType(String finalStr, String type, String name, String value, int depth) 
 			throws SJavacException {
 		if(finalStr != null){
@@ -30,6 +46,14 @@ public class TypeFactory {
 		throw new InvalidTypeException("Invalid type for variable declaration.");
 	}
 	
+	/** This method returns a the correct Type of a method argument, 
+	 * for the purpose of validating whether the args are valid or not.
+	 * @param finalStr substring where "final" would be
+	 * @param type substring where the type is defined
+	 * @return a reference to an instance of the correct subclass of Type.
+	 * @throws InvalidTypeException is thrown when the String type doesn't match any
+	 * of the accepted type patterns.
+	 */
 	public Type generateMethodParamType(String finalStr, String type) throws InvalidTypeException{
 		if(finalStr != null){
 			isFinal = true;
