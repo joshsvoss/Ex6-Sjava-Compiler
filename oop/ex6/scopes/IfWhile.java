5c639d4417d9706aca6ef4d947ab16c223b45d84
shana-m <shana.milstein@gmail.com>
2015-06-25 20:11:01 +0300

    fixed error: if while now checks variable name to make sure the associated variable is a valid condition type (and that it exists.)

--- oop/ex6/scopes/IfWhile.java
+++ oop/ex6/scopes/IfWhile.java
@@ -3,14 +3,20 @@ package oop.ex6.scopes;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import oop.ex6.main.Parser;
+import oop.ex6.types.Int;
 import oop.ex6.types.InvalidValueException;
+import oop.ex6.types.Type;
+import oop.ex6.types.Double;
+import oop.ex6.types.Boolean;
 
 public class IfWhile extends Scope{
 	
 	//TODO We don't need to have an ifWhile object do we, so maybe we should just have a static 
 	// TODO "checkIFWhile()" method
 	
-	private Pattern legalConditions = Pattern.compile("true|false|([a-zA-Z_]{1}+\\w*)|(-?+[0-9]++(\\.{1}+[0-9]+)?)");
+	private Pattern booleanIntOrDouble = Pattern.compile("true|false|(-?+[0-9]++(\\.{1}+[0-9]+)?)");
+	private Pattern legalVarName = Pattern.compile("([a-zA-Z_]{1}+\\w*)");
 	private String booleanOperatorSeparator = "\\s*([|][|]){1}\\s*|\\s*([&][&]){1}\\s*";
 	private String[] conditions;
 
@@ -39,9 +45,21 @@ public class IfWhile extends Scope{
 
 	@Override
 	public boolean checkParamLogic(String condition) throws InvalidValueException {
-		Matcher legalConditionMatch = legalConditions.matcher(condition);
-		if(legalConditionMatch.matches()){
+		Matcher boolenaIntOrDoubleMatch = booleanIntOrDouble.matcher(condition);
+		Matcher legalVarNameMatch = legalVarName.matcher(condition);
+		if(boolenaIntOrDoubleMatch.matches()){
 			return true;
+		}else if(legalVarNameMatch.matches()){
+			for (int i = this.depth; i >= 0; i--) {
+				Type conditionVar = Parser.getSymbolTableList().elementAt(i).get(condition);
+				if(conditionVar != null){
+					if((conditionVar instanceof Int) || (conditionVar instanceof Double) || 
+							(conditionVar instanceof Boolean)){
+						return true;
+					}
+					break;
+				}
+			}
 		}
 		throw new InvalidValueException("Invalid condition for an if/while block.");
 	}
