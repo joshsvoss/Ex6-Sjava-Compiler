80e18bb625d12791c4432fc45efed9f71689f49f
shana-m <shana.milstein@gmail.com>
2015-06-27 23:32:23 +0300

    more documentation..

--- oop/ex6/types/FinalVariableException.java
+++ oop/ex6/types/FinalVariableException.java
@@ -2,16 +2,27 @@ package oop.ex6.types;
 
 import oop.ex6.main.SJavacException;
 
+/**This exception is thrown when a final variable is reassigned.
+ * 
+ * @author Shana M, Joshsvoss
+ *
+ */
 public class FinalVariableException extends SJavacException {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private static final String defaultMsg = "A final variable cannot be reassigned.";
-
+	
+	/** 
+	 * Identical to parents constructor.
+	 */
 	public FinalVariableException() {
 		super(defaultMsg);
 	}
-
+	
+	/** Identical to that of super.  
+	 * @param msg message to be passed up with exception.
+	 */
 	public FinalVariableException(String msg) {
 		super(msg);
 	}
--- oop/ex6/types/Int.java
+++ oop/ex6/types/Int.java
@@ -4,15 +4,40 @@ package oop.ex6.types;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
+/**This is the class for the Int Type variable.
+ * 
+ * @author Shana M, joshsvoss
+ *
+ */
 public class Int extends Type {
 	
-	private static final Pattern intRegex = Pattern.compile("-?[0-9]+"); //TODO Magic number?
+	// String used to create the legal int values regex.
+	private static final String LEGAL_INT_VALUES_STRING = "-?[0-9]+";
 	
+	// The regex for the legal int values.
+	private static final Pattern LEGAL_INT_VALUES_REGEX = Pattern.compile(LEGAL_INT_VALUES_STRING);
+	
+	/** This constructor is for making Int Type instances out of method declaration's parameters.
+	 * 
+	 * @param isFinal this is passed as true
+	 * @param name the name of the variable.
+	 */
 	public Int(boolean isFinal, String name){
 		super(isFinal, name);
 	}
 	
+	/**The constructor for creating a Int Type variable, upon declaration.
+	 * 
+	 * @param name the name of the variable.
+	 * @param value the value of the variable.
+	 * @param depth the depth at which the variable was declared.
+	 * @param isFinal keeps track of whether the variable is final.
+	 * @throws InvalidValueException thrown when an illegal value is assigned to the variable.
+	 * @throws UninitializedFinalVariableException thrown when a final variable is declared without
+	 * initialization.
+	 * @throws AssignmentFromUninitializedVarException thrown when the an uninitialized variable is assigned 
+	 * to a different variable.
+	 */
 	public Int(String name, String value, int depth, boolean isFinal) throws InvalidValueException, 
 	UninitializedFinalVariableException, AssignmentFromUninitializedVarException {
 		super(name, value, depth, isFinal);
@@ -22,7 +47,7 @@ public class Int extends Type {
 	@Override
 	public boolean doesValueMatchType(String value) throws InvalidValueException {
 		
-		Matcher intMatch = intRegex.matcher(value);
+		Matcher intMatch = LEGAL_INT_VALUES_REGEX.matcher(value);
 		if(intMatch.matches()){
 			return true;
 		}
